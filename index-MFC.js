8a19f41243540e5f14611c1db43fe28f6b780019
Marta Fernández <martaafdez07@gmail.com>
2024-02-19 14:11:42 +0100

    index Marta

--- index-MFC.js
+++ index-MFC.js
@@ -1,93 +1,189 @@
 //console.log("Hola Mundo");
 
 
-
+/*
 let country = ["Portugal","China"," Denmark","France","France","United States","France","Poland","China","France",
 "China","Denmark","Philippines","France","Iran","Canada","Denmark","Armenia","China"];
 
 let hours = [17, 14, 1, 22, 5, 9, 23, 21, 18, 10, 11, 22, 13, 4, 11, 3, 1, 1, 7, 9, 7];
+*/
 
-
-// Creamos un objeto para contar la frecuencia de aparición de cada país
-const countCountries = {};
-country.forEach((pais) => {
-  countCountries[pais] = (countCountries[pais] || 0) + 1;
-});
-
-// Filtramos los países que aparecen más de una vez
-const paisesRepetidos = Object.keys(countCountries).filter((pais) => countCountries[pais] > 1);
-
-// Inicializamos un objeto para almacenar las sumas y contar la cantidad de horas para cada país repetido
-const sumasHoras = {};
-const counts = {};
-paisesRepetidos.forEach((pais) => {
-  sumasHoras[pais] = 0;
-  counts[pais] = 0;
-});
-
-// Sumamos las horas para cada país repetido
-for (let i = 0; i < country.length; i++) {
-  const pais = country[i];
-  if (paisesRepetidos.includes(pais)) {
-    sumasHoras[pais] += hours[i];
-    counts[pais]++;
+  
+  const datos = [
+    {
+      "student_age": 25,
+      "sex": "Male",
+      "additional_work": "Yes",
+      "sports_activity": "No",
+      "transportation": "Private",
+      "weekly_study_hours": 17,
+      "reading": "Yes",
+      "listening_in_class": "No",
+      "project_work": "No",
+      "attendance_percentage": 84,
+      "calification_average": 3.5,
+      "country": "China",
+      "date": "09/04/2023"
+  },
+  {
+      "student_age": 25,
+      "sex": "Male",
+      "additional_work": "Yes",
+      "sports_activity": "No",
+      "transportation": "Private",
+      "weekly_study_hours": 14,
+      "reading": "Yes",
+      "listening_in_class": "Yes",
+      "project_work": "Yes",
+      "attendance_percentage": 97,
+      "calification_average": 3,
+      "country": "China",
+      "date": "3/20/2023"
+  },
+  {
+      "student_age": 19,
+      "sex": "Male",
+      "additional_work": "No",
+      "sports_activity": "No",
+      "transportation": "Private",
+      "weekly_study_hours": 1,
+      "reading": "No",
+      "listening_in_class": "No",
+      "project_work": "Yes",
+      "attendance_percentage": 79,
+      "calification_average": 1,
+      "country": "Portugal",
+      "date": "10/29/2023"
+  },
+  {
+      "student_age": 20,
+      "sex": "Female",
+      "additional_work": "Yes",
+      "sports_activity": "No",
+      "transportation": "Bus",
+      "weekly_study_hours": 22,
+      "reading": "No",
+      "listening_in_class": "No",
+      "project_work": "No",
+      "attendance_percentage": 93,
+      "calification_average": 4,
+      "country": "China",
+      "date": "02/03/2024"
+  },
+  {
+      "student_age": 25,
+      "sex": "Male",
+      "additional_work": "No",
+      "sports_activity": "No",
+      "transportation": "Bus",
+      "weekly_study_hours": 5,
+      "reading": "Yes",
+      "listening_in_class": "Yes",
+      "project_work": "Yes",
+      "attendance_percentage": 96,
+      "calification_average": 1,
+      "country": "Denmark",
+      "date": "5/14/2023"
+  },
+  {
+      "student_age": 24,
+      "sex": "Male",
+      "additional_work": "No",
+      "sports_activity": "No",
+      "transportation": "Private",
+      "weekly_study_hours": 9,
+      "reading": "Yes",
+      "listening_in_class": "Yes",
+      "project_work": "Yes",
+      "attendance_percentage": 80,
+      "calification_average": 2,
+      "country": "France",
+      "date": "03/06/2023"
+  },
+  {
+      "student_age": 20,
+      "sex": "Male",
+      "additional_work": "No",
+      "sports_activity": "No",
+      "transportation": "Private",
+      "weekly_study_hours": 23,
+      "reading": "No",
+      "listening_in_class": "Yes",
+      "project_work": "Yes",
+      "attendance_percentage": 86,
+      "calification_average": 4,
+      "country": "France",
+      "date": "07/08/2023"
+  },
+  {
+      "student_age": 20,
+      "sex": "Female",
+      "additional_work": "Yes",
+      "sports_activity": "Yes",
+      "transportation": "Bus",
+      "weekly_study_hours": 21,
+      "reading": "No",
+      "listening_in_class": "Yes",
+      "project_work": "Yes",
+      "attendance_percentage": 97,
+      "calification_average": 4,
+      "country": "United States",
+      "date": "1/20/2024"
+  },
+  {
+      "student_age": 20,
+      "sex": "Female",
+      "additional_work": "No",
+      "sports_activity": "Yes",
+      "transportation": "Bus",
+      "weekly_study_hours": 18,
+      "reading": "No",
+      "listening_in_class": "No",
+      "project_work": "Yes",
+      "attendance_percentage": 89,
+      "calification_average": 3.5,
+      "country": "France",
+      "date": "2/23/2023"
+  },
+  {
+      "student_age": 21,
+      "sex": "Female",
+      "additional_work": "No",
+      "sports_activity": "No",
+      "transportation": "Bus",
+      "weekly_study_hours": 10,
+      "reading": "No",
+      "listening_in_class": "No",
+      "project_work": "No",
+      "attendance_percentage": 96,
+      "calification_average": 2,
+      "country": "Poland",
+      "date": "11/22/2023"
   }
-}
+  ];
+  
+  // Filtrar países que aparecen más de una vez
+  const paisesRepetidos = datos.reduce((acc, dato) => {
+    if (acc[dato.country]) {
+      acc[dato.country]++;
+    } else {
+      acc[dato.country] = 1;
+    }
+    return acc;
+  }, {});
+  
+  const paisesConMasDeUnaAparicion = Object.keys(paisesRepetidos).filter(pais => paisesRepetidos[pais] > 1);
+  
+  // Calcular la media de las horas de estudio para los países seleccionados
+  const horasDeEstudio = paisesConMasDeUnaAparicion.map(pais => {
+    const filasFiltradas = datos.filter((fila) => fila.country === pais);
+    const horasEstudio = filasFiltradas.map((fila) => fila.weekly_study_hours);
+    const mediaHorasEstudio = horasEstudio.reduce((acc, valor) => acc + valor, 0) / horasEstudio.length;
+    return { pais, mediaHorasEstudio };
+  });
+  
+  
 
-// Calculamos las medias para cada país repetido
-const medias = {};
-paisesRepetidos.forEach((pais) => {
-  medias[pais] = sumasHoras[pais] / counts[pais];
-});
-
-console.log("Media de horas por país que aparece más de una vez:");
-console.log(medias);
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
+  
+console.log(horasDeEstudio);
