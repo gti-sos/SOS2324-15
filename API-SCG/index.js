f095d1ba20dd39a09be03e532842dea3c315fd97
SergioKenz0 <serguky@hotmail.com>
2024-03-11 13:32:12 +0100

    arreglo f05

--- API-SCG/index.js
+++ API-SCG/index.js
@@ -58,38 +58,84 @@ let data_SCG = require('../index-SCG');
 
   
 // Get sobre api con paginación y filtros
-app.get(API_BASE, (req, res) => {
-  // Obtener parámetros de consulta
-  const filters = req.query;
-  const limit = parseInt(filters.limit) || 10;  // Valor por defecto de 10 si no se proporciona
 
-  // Construir el objeto de filtro basado en los parámetros de consulta
-  const filterObject = {};
 
-  // Iterar sobre los parámetros de la consulta y agregarlos al objeto de filtro
-  Object.keys(filters).forEach(key => {
-    if (key !== 'limit') {
-      // Verificar si el valor es numérico
-      const value = isNaN(filters[key]) ? filters[key] : parseInt(filters[key]);
+// app.get(API_BASE, (req, res) => {
+//   // Obtener parámetros de consulta
+//   const filters = req.query;
+//   const limit = parseInt(filters.limit) || 10;  // Valor por defecto de 10 si no se proporciona
 
-      filterObject[key] = value;
-    }
-  });
+//   // Construir el objeto de filtro basado en los parámetros de consulta
+//   const filterObject = {};
 
-  // Realizar la búsqueda en la base de datos con los filtros y paginación
-  dbSleep.find(filterObject)
-    .limit(limit)
-    .exec((err, filteredData) => {
-      if (err) {
-        res.status(500).json({ message: 'Internal Error' });
-      } else {
-        // Si no hay error, devolver los datos filtrados
-        res.status(200).json(filteredData);
-      }
+//   // Iterar sobre los parámetros de la consulta y agregarlos al objeto de filtro
+//   Object.keys(filters).forEach(key => {
+//     if (key !== 'limit') {
+//       // Verificar si el valor es numérico
+//       const value = isNaN(filters[key]) ? filters[key] : parseInt(filters[key]);
+
+//       filterObject[key] = value;
+//     }
+//   });
+
+//   // Realizar la búsqueda en la base de datos con los filtros y paginación
+//   dbSleep.find(filterObject)
+//     .limit(limit)
+//     .exec((err, filteredData) => {
+//       if (err) {
+//         res.status(500).json({ message: 'Internal Error' });
+//       } else {
+//         // Si no hay error, devolver los datos filtrados
+//         res.status(200).json(filteredData);
+//       }
+//     });
+// });
+
+  
+  //GET GENERAL 
+
+  // Búsqueda de datos con parámetros específicos y paginación
+  app.get(API_BASE, (req, res) => {
+    // Obtenemos los parámetros de búsqueda y paginación de la solicitud
+    const queryParameters = req.query;
+    const limit = parseInt(queryParameters.limit) || 10; // Tamaño de página predeterminado: 10
+    const offset = parseInt(queryParameters.offset) || 0; // Offset predeterminado: 0
+
+    // Construimos la consulta de búsqueda basada en los parámetros proporcionados
+    let query = {};
+
+    // Iteramos sobre cada parámetro de búsqueda
+    Object.keys(queryParameters).forEach(key => {
+        // Si el parámetro no es "limit", "offset" u otros parámetros de paginación, lo consideramos como un atributo de búsqueda
+        if (key !== 'limit' && key !== 'offset') {
+            // Verificamos si el valor es numérico
+            const value = !isNaN(queryParameters[key]) ? parseInt(queryParameters[key]) : queryParameters[key];
+            // Si es numérico, agregamos un filtro de igualdad, de lo contrario, realizamos la búsqueda de texto como antes
+            query[key] = !isNaN(value) ? value : new RegExp(value, 'i');
+        }
+    });
+
+    // Ejecutamos la consulta en la base de datos con paginación
+    dbSleep.find(query).skip(offset).limit(limit).exec((err, datosSleep) => {
+        if (err) {
+            res.status(500).json({ message: 'Internal Error' });
+        } else {
+            // Eliminamos el campo _id de los resultados
+            const resultsWithoutId = datosSleep.map(student => {
+                const { _id, ...studentWithoutId } = student;
+                return studentWithoutId;
+            });
+            res.status(200).json(resultsWithoutId);
+        }
     });
 });
 
-  
+
+
+
+
+
+
 
 
  
@@ -227,30 +273,56 @@ app.get(API_BASE, (req, res) => {
 
   //Put sobre un valor de country
 
-  app.put(API_BASE + "/:country", (req, res) => {
-    const countryNameURL = req.params.country;
-    const newData = req.body;
-    const countryNameBody = newData.country;
+  // app.put(API_BASE + "/:country", (req, res) => {
+  //   const countryNameURL = req.params.country;
+  //   const newData = req.body;
+  //   const countryNameBody = newData.country;
   
-    // Verifica si la country en la URL coincide con la del body
-    if (countryNameURL !== countryNameBody) {
-      res.status(400).json({ message: 'Country mismatch between URL and body' });
-      return;
-    }
+  //   // Verifica si la country en la URL coincide con la del body
+  //   if (countryNameURL !== countryNameBody) {
+  //     res.status(400).json({ message: 'Country mismatch between URL and body' });
+  //     return;
+  //   }
     
-    // Actualiza el documento en la base de datos
-    dbSleep.update({ country: countryNameURL }, newData, {}, (err, numUpdated) => {
-      if (err) {
-        res.status(500).json({ message: 'Internal Error' });
+  //   // Actualiza el documento en la base de datos
+  //   dbSleep.update({ country: countryNameURL }, newData, {}, (err, numUpdated) => {
+  //     if (err) {
+  //       res.status(500).json({ message: 'Internal Error' });
+  //     } else {
+  //       if (numUpdated === 1) {
+  //         res.status(200).json({ message: 'Updated', updatedData: newData });
+  //       } else {
+  //         res.status(404).json({ message: 'Country not found' });
+  //       }
+  //     }
+  //   });
+  // });
+
+  // PUT para actualizar datos de un país específico
+app.put(API_BASE + "/:country", (req, res) => {
+  const countryNameURL = req.params.country;
+  const newData = req.body;
+  const countryNameBody = newData.country;
+
+  // Verifica si la country en la URL coincide con la del body
+  if (countryNameURL !== countryNameBody) {
+    res.status(400).json({ message: 'Country mismatch between URL and body' });
+    return;
+  }
+  
+  // Actualiza el documento en la base de datos
+  dbSleep.update({ country: countryNameURL }, newData, {}, (err, numUpdated) => {
+    if (err) {
+      res.status(500).json({ message: 'Internal Error' });
+    } else {
+      if (numUpdated === 1) {
+        res.status(200).json({ message: 'Updated', updatedData: newData });
       } else {
-        if (numUpdated === 1) {
-          res.status(200).json({ message: 'Updated', updatedData: newData });
-        } else {
-          res.status(404).json({ message: 'Country not found' });
-        }
+        res.status(404).json({ message: 'Country not found' });
       }
-    });
+    }
   });
+});
 
 
   //Delete sobre valor de country
